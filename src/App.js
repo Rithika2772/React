0f8eb19ff1617df37fd84d0b3e5537ab08aaa8bd
Rithika <rithikapoojary07@gmail.com>
2023-02-14 11:00:42 +0530

    rithi_react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Image from "./assets/img_forest.jpg"
 
 function App() {
-  return (
+  const x=10;
+  var text;
+  if(x===1){
+    text="Equal";
+  }else{
+    text="Not Equal";
+  }
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <h2>{text}</h2>
+      <h2>{x>=10 ? "greater": "lesser"}</h2>
+      <p>this is paragraph</p>
+      <h3>{5+50}</h3>
+      <img src={Image} alt="pic"/>
     </div>
   );
 }
-
 export default App;
